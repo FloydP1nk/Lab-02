504335d7d821a534a6da6185d38154b1ef644db5
FloydP1nk <113801739+FloydP1nk@users.noreply.github.com>
2023-03-09 03:09:59 +0300

    formated

--- hw.cpp
+++ hw.cpp
@@ -1,7 +1,10 @@
 #include "iostream"
 #include "string"
-int main() {
-    std::string userName; // user name
-    std::cin >> userName; // input
-    std::cout << "hello world from " << userName << std::endl; // output
+int
+main()
+{
+  std::string userName;                                      // user name
+  std::cin >> userName;                                      // input
+  std::cout << "hello world from " << userName << std::endl; // output
 }
+
